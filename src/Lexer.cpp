e3d96c1d944bcd1ccb2f3671048791e4e215083f
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-09-01 21:27:14 -0300

    feat: peek and lex numbers and literals implementation
    TODO: figure out if the lex_numbers are right

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -5,8 +5,11 @@
 #include <stack>
 #include <string>
 
+#include "Lexer/Kind.hpp"
 #include "Lexer/Lexer.hpp"
 #include "Lexer/Token.hpp"
+
+#include "Print.hpp"
 
 Lexer::Lexer()
     : m_line(0)
@@ -40,6 +43,16 @@ auto Lexer::step() -> bool
     return true;
 }
 
+auto Lexer::peek(std::size_t n = 0) const -> std::optional<char>
+{
+    if ((m_index + n) < m_source.length())
+    {
+        return m_source.at(m_index + n);
+    }
+
+    return {};
+}
+
 auto Lexer::is_space() -> void
 {
     if (current_char() == '\n')
@@ -65,6 +78,96 @@ auto Lexer::push_token(Token t) -> void
     m_tokens.push_back(t);
 }
 
+enum class State
+{
+    START,
+    LEX_NUMBERS,
+    FINISH,
+};
+
+auto Lexer::lex_numbers() -> Token
+{
+    State s = State::START;
+
+    while (s != State::FINISH)
+    {
+        switch (s)
+        {
+            case State::START: {
+                switch (char c = current_char())
+                {
+                    case '0' ... '9':
+                        s = State::LEX_NUMBERS;
+                        break;
+                    default:
+                        s = State::FINISH;
+                        break;
+                }
+            }
+            break;
+            case State::LEX_NUMBERS: {
+                switch (char c = current_char())
+                {
+                    case '0': {
+                        if (peek(1).value() == 'x')
+                        {
+                            expr::print("Lexing a hex number\n");
+                            // TODO: change the state to the lex_hex impl
+                            s = State::FINISH;
+                        }
+                    }
+                    case '1' ... '9':
+                        step();
+                        expr::print("Number: {}\n", c);
+                        break;
+                    default:
+                        s = State::FINISH;
+                        break;
+                }
+            }
+            break;
+
+            case State::FINISH:
+                s = State::FINISH;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    auto err =
+        std::format("[Lexer] - Invalid token with value: {}", current_char());
+
+    // TODO: find a better way to deal with errors
+    return Token(kind_t::ERROR, err, m_line, m_index);
+}
+
+auto Lexer::lex_literals() -> Token
+{
+    switch (char c = current_char())
+    {
+        case '+':
+        case '-':
+        case '/':
+        case '*':
+        case '^':
+            step();
+            return Token(LITERAL_TO_KIND.at(c), std::string(1, c), m_line,
+                         m_index);
+            break;
+
+        default:
+            expr::print("Invalid Literal on Line: {} and Column: {} -> {}\n",
+                        m_line, m_index, c);
+            break;
+    }
+
+    // TODO: find a better way to deal with errors
+    return Token(kind_t::ERROR, "Invalid token from lex_literals", m_line,
+                 m_index);
+}
+
 auto Lexer::next_token() -> Token
 {
     while (!is_empty())
@@ -79,48 +182,23 @@ auto Lexer::next_token() -> Token
                 break;
 
             case '0' ... '9':
-                step();
-                return Token(kind_t::NUMBER, (c - '0'), m_line, m_index);
+                return lex_numbers();
                 break;
 
-            case '*':
-                step();
-                return Token(kind_t::MULTIPLICATION, std::string(1, c), m_line,
-                             m_index);
-                break;
-            case '/':
-                step();
-                return Token(kind_t::DIVISION, std::string(1, c), m_line,
-                             m_index);
-                break;
             case '+':
-                step();
-                return Token(kind_t::ADDITION, std::string(1, c), m_line,
-                             m_index);
-                break;
             case '-':
-                step();
-                return Token(kind_t::SUBTRACTION, std::string(1, c), m_line,
-                             m_index);
-                break;
-
+            case '*':
+            case '/':
+            case '^':
             case '(':
-                step();
-                return Token(kind_t::OPENPAREN, std::string(1, c), m_line,
-                             m_index);
-                break;
-
             case ')':
-                step();
-                return Token(kind_t::CLOSEPAREN, std::string(1, c), m_line,
-                             m_index);
+                return lex_literals();
                 break;
 
             default:
                 step();
-                std::cout << std::format(
-                    "[Lexer] - Line: {} Column: {} Current: {}\n", m_line,
-                    m_index, c);
+                expr::print("[Lexer] - Line: {} Column: {} -> {}\n", m_line,
+                            m_index, c);
                 break;
         }
     }
