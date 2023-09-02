6449d51037bd27e7c4a518b72fc80b2b5422162e
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-09-01 21:26:00 -0300

    feat: add literals to kind_t
    also added an error type

--- include/Expr/Lexer/Kind.hpp
+++ include/Expr/Lexer/Kind.hpp
@@ -10,12 +10,15 @@ enum class kind_t
     DIVISION,
     ADDITION,
     SUBTRACTION,
+    POWER,
+    MODULUS,
     OPENPAREN,
     CLOSEPAREN,
+    ERROR,
     ENDOFFILE,
 };
 
-const std::unordered_map<kind_t, std::string> KIND_STR = {
+const std::unordered_map<kind_t, std::string> KIND_TO_STR = {
     { kind_t::NUMBER, "Number" },
     { kind_t::MULTIPLICATION, "Multiplication" },
     { kind_t::DIVISION, "Division" },
@@ -23,7 +26,15 @@ const std::unordered_map<kind_t, std::string> KIND_STR = {
     { kind_t::SUBTRACTION, "Subtraction" },
     { kind_t::OPENPAREN, "Openparen" },
     { kind_t::CLOSEPAREN, "Closeparen" },
+    { kind_t::ERROR, "Error" },
     { kind_t::ENDOFFILE, "EOF" },
+};
+
+const std::unordered_map<char, kind_t> LITERAL_TO_KIND = {
+    { '+', kind_t::ADDITION },       { '-', kind_t::SUBTRACTION },
+    { '*', kind_t::MULTIPLICATION }, { '/', kind_t::DIVISION },
+    { '%', kind_t::MODULUS },        { '^', kind_t::ADDITION },
+    { '(', kind_t::OPENPAREN },      { ')', kind_t::CLOSEPAREN },
 };
 
 class Kind
