efac98fbe1e57424d13b1860101d6f6ef9afe660
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-09-23 12:15:41 -0300

    feat: add some util functions to the kind class also refactored all the kind_t enum and its uses

--- include/Expr/Lexer/Kind.hpp
+++ include/Expr/Lexer/Kind.hpp
@@ -5,44 +5,22 @@
 
 enum class kind_t
 {
-    NUMBER,
-    MULTIPLICATION,
-    DIVISION,
-    ADDITION,
-    SUBTRACTION,
-    POWER,
-    MODULUS,
-    OPENPAREN,
-    CLOSEPAREN,
-    ERROR,
-    ENDOFFILE,
-};
-
-const std::unordered_map<kind_t, std::string> KIND_TO_STR = {
-    { kind_t::NUMBER, "Number" },
-    { kind_t::MULTIPLICATION, "Multiplication" },
-    { kind_t::DIVISION, "Division" },
-    { kind_t::ADDITION, "Addition" },
-    { kind_t::SUBTRACTION, "Subtraction" },
-    { kind_t::OPENPAREN, "Openparen" },
-    { kind_t::CLOSEPAREN, "Closeparen" },
-    { kind_t::ERROR, "Error" },
-    { kind_t::ENDOFFILE, "EOF" },
-};
-
-const std::unordered_map<char, kind_t> CHAR_TO_KIND = {
-    { '+', kind_t::ADDITION },       { '-', kind_t::SUBTRACTION },
-    { '*', kind_t::MULTIPLICATION }, { '/', kind_t::DIVISION },
-    { '%', kind_t::MODULUS },        { '^', kind_t::ADDITION },
-    { '(', kind_t::OPENPAREN },      { ')', kind_t::CLOSEPAREN },
+    __EOF = 0,
+    __LAST_VALID_CHAR = 127, // Reserve first 128 values for one-char tokens
+    TOKEN_NUMBER,
+    TOKEN_ERROR,
 };
 
 class Kind
 {
 public:
+    Kind(char);
     Kind(kind_t);
 
     [[nodiscard]] auto raw() const -> kind_t;
+
+    [[nodiscard]] auto as_int() const -> int;
+    [[nodiscard]] auto as_int(kind_t) const -> int;
     [[nodiscard]] auto as_string() const -> std::string;
 
 private:
--- src/Lexer/Kind.cpp
+++ src/Lexer/Kind.cpp
@@ -1,4 +1,17 @@
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
 #include "Lexer/Kind.hpp"
+
+const std::unordered_map<kind_t, std::string> kind_as_string = {
+    { kind_t::TOKEN_NUMBER, "Number" },
+    { kind_t::TOKEN_ERROR, "Number" },
+};
+
+Kind::Kind(char kind)
+    : m_kind(static_cast<kind_t>(kind))
+{}
 
 Kind::Kind(kind_t kind)
     : m_kind(kind)
@@ -9,7 +22,24 @@ auto Kind::raw() const -> kind_t
     return m_kind;
 }
 
+auto Kind::as_int() const -> int
+{
+    return static_cast<int>(m_kind);
+}
+
+auto Kind::as_int(kind_t kind) const -> int
+{
+    return static_cast<int>(kind);
+}
+
 auto Kind::as_string() const -> std::string
 {
-    return KIND_TO_STR.at(m_kind);
+    if (as_int(m_kind) >= 128)
+    {
+        return kind_as_string.at(m_kind);
+    }
+    else
+    {
+        return std::string(1, as_int(m_kind));
+    }
 }
