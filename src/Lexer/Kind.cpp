b281b4d4160881b3b2d17f1289e7d4a4fc78942a
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-10-04 09:20:04 -0300

    refactore: changed the name of some kinds

--- src/Lexer/Kind.cpp
+++ src/Lexer/Kind.cpp
@@ -5,15 +5,15 @@
 #include "Lexer/Kind.hpp"
 
 const std::unordered_map<kind_t, std::string> kind_as_string = {
-    { kind_t::LPAREN, "LParen" },
-    { kind_t::RPAREN, "RParen" },
+    { kind_t::LPAREN, "LeftParen" },
+    { kind_t::RPAREN, "RightParen" },
     { kind_t::PLUS, "Plus" },
     { kind_t::MINUS, "Minus" },
     { kind_t::SLASH, "Slash" },
     { kind_t::STAR, "Star" },
     { kind_t::PERCENTAGE, "Percentage" },
     { kind_t::CARROT, "Carrot" },
-    { kind_t::NUMBERLIT, "NumberLiteral" },
+    { kind_t::NUMBERLIT, "NumberLit" },
     { kind_t::ERROR, "Error" },
 };
 
