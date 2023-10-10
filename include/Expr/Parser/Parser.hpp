45323ad8bb53402b8bc2201601ab821db1d7d4f1
Marcelo Garoze <marcelogaroze@hotmail.com>
2023-10-10 15:28:14 -0300

    refactore: removed the symbol_table from the parser

--- include/Expr/Parser/Parser.hpp
+++ include/Expr/Parser/Parser.hpp
@@ -5,7 +5,6 @@
 #include <cwchar>
 #include <memory>
 #include <optional>
-#include <unordered_map>
 #include <vector>
 
 #include "Lexer/Kind.hpp"
@@ -40,5 +39,4 @@ private:
 private:
     std::size_t m_index;
     std::vector<Token> m_tokens;
-    std::unordered_map<std::string, double> m_symbol_table;
 };
